d8c4699adb92a34a70f7feb9bebf214b6e303152
YabaiNyan <netmin@desuu.com>
2019-01-19 16:06:28 +1100

    fix multi touch on elements

--- html/script.js
+++ html/script.js
@@ -4,12 +4,20 @@ ws.onmessage = function (event) {
     console.log(event.data);
 };
 
-function btdown(key, ele){
-    ws.send("<"+key)
-    ele.style.backgroundImage="url('tap.png'), url('idle.png')"
+function btdown(key, e, ele){
+    if(e.targetTouches.length == 1){
+        ws.send("<"+key)
+        ele.style.backgroundImage="url('tap.png'), url('idle.png')"
+    }
 }
 
-function btup(key, ele){
-    ws.send(">"+key)
-    ele.style.backgroundImage="url('idle.png')"
+function btup(key, e, ele){
+    if(e.targetTouches.length == 0){
+        ws.send(">"+key)zzzzzzzz
+        ele.style.backgroundImage="url('idle.png')"
+    }
+}
+
+function rlog(a){
+    ws.send("!"+a)
 }
